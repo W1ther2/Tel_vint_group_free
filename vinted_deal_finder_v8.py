38649342dea434525943d3dd6569ff4f7f8c4d7c
W1ther2 <ainius226@gmail.com>
2026-09-08 14:32:23 +0300

    Update vinted_deal_finder_v8.py

--- vinted_deal_finder_v8.py
+++ vinted_deal_finder_v8.py
@@ -25,9 +25,6 @@ TOPIC_ID = 1
 DEFAULTS = {
     "MODELS": [
         {"query": "iPhone 13",     "min_price": 100, "max_price": 160},
-        {"query": "iPhone 13 Pro", "min_price": 100, "max_price": 200},
-        {"query": "iPhone 14",     "min_price": 100, "max_price": 200},
-        {"query": "iPhone 14 Pro", "min_price": 100, "max_price": 350},
     ],
     "BLACKLIST_WORDS": [
         "case", "deklas", "cover", "custodia", "coque",
